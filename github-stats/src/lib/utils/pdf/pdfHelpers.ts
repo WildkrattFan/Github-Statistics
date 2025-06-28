9082d577450ca4808b42e8ddeb8f7c9b6ebc6b6d
WildkrattFan <88059692+WildkrattFan@users.noreply.github.com>
2025-06-28 17:00:31 -0500

    taking a break from pdf parsing

--- github-stats/src/lib/utils/pdf/pdfHelpers.ts
+++ github-stats/src/lib/utils/pdf/pdfHelpers.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs';
-import pdfParse from 'pdf-parse';
+// import * as fs from 'fs';
 
 export async function extractTextFromPDF(file: File): Promise<string> {
+    const pdfParse = (await import('pdf-parse')).default;
     const abuffer = await file.arrayBuffer();
     const buffer = Buffer.from(abuffer);
     const data = await pdfParse(buffer);
